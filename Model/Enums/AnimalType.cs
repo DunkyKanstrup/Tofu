c875688ebc54c382149235b33517c516522a6c5a
Kanstrup <krjoh21@student.sdu.dk>
2023-06-08 12:09:03 +0200

    Models and enums modified

---
+++ Model/Enums/AnimalType.cs
@@ -0,0 +1,11 @@
+﻿namespace Tofu.Model.Enums
+{
+    public enum AnimalType
+    {
+        Cow,
+        Pig,
+        Horse,
+        Goat,
+        Deer
+    }
+}
---
+++ obj/Debug/net7.0-android/android/src/crc640c0c0f4b157ec0bd/MainActivity.java
@@ -0,0 +1,48 @@
+package crc640c0c0f4b157ec0bd;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Tofu.MainActivity, Tofu", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Tofu.MainActivity, Tofu", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Tofu.MainActivity, Tofu", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
